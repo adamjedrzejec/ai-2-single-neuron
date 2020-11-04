2c3f40b74be3c13daf0e86dfe2adf79f59ce8f81
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-11-04 20:24:03 +0100

    [add] 4 activation functions with derivatives

--- functions-derivatives.py
+++ functions-derivatives.py
@@ -21,9 +21,53 @@ def logisticFunctionDerivative(x):
     return logisticFunction(x) * (1 - logisticFunction(x))
 
 
+def sinh(x):
+    return math.sinh(x)
+
+
+def sinhDerivative(x):
+    return math.cosh(x)
+
+
 def tanh(x):
     return math.tanh(x)
 
 
 def tanhDerivative(x):
     return 1.0 - tanh(x) ** 2
+
+
+def sign(x):
+    if x < 0:
+        return -1
+    elif x > 0:
+        return 1
+    return 0
+
+
+def signDerivative(x):
+    return 1
+
+
+def reLu(x):
+    if x > 0:
+        return x
+    return 0
+
+
+def reLuDerivative(x):
+    if x > 0:
+        return 1
+    return 0
+
+
+def leakyReLu(x):
+    if x > 0:
+        return x
+    return 0.01 * x
+
+
+def leakyReLuDerivative(x):
+    if x > 0:
+        return 1
+    return 0.01
