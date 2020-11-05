34fa4ba912886f16fb0e4d5ef8c1e0e4a52043fe
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-11-05 15:44:25 +0100

    [add] 2 classifiers simple training

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+import numpy as np
+
 import neuron
 from neuron import ActivationFunctionTypes as aft
 import classifier as c
 
-neu = neuron.Neuron([1, 0.5], aft.LogisticFunction)
+neu = neuron.Neuron([1, 0.5], aft.HeaviSideStepFunction)
 
-for i in range(10):
-    neu.train([1, -1], 0.25)
-    neu.updateWeights()
-    print()
+# for i in range(10):
+#     neu.train([1, -1], 0.25)
+#     neu.updateWeights()
+#     print()
 
-c1 = c.Classifier(2, 5)
-x, y = c1.getAllSamples()
+c1 = c.Classifier(2, 400)
+x1, y1 = c1.getAllSamples()
 
-print(x, y)
+li1 = list(zip(x1, y1))
+
+for i in range(200):
+    for index, trainingTouple in enumerate(li1):
+        # print(index, trainingTouple)
+        neu.train(trainingTouple, 0)
+        neu.updateWeights()
+
+
+c2 = c.Classifier(2, 400)
+x2, y2 = c2.getAllSamples()
+
+li2 = list(zip(x2, y2))
+for i in range(200):
+    for index, trainingTouple in enumerate(li2):
+        # print(index, trainingTouple)
+        neu.train(trainingTouple, 1)
+        neu.updateWeights()
+
+
+print('shouldBeTrue', neu.examine(li1[2], 0))
+print('shouldBeTrue', neu.examine(li2[2], 1))
