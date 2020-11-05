067afd1a8ee5fdd418173183fc6a37b361da674f
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-11-05 01:48:02 +0100

    [remove] unnecessary comment

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import neuron
 from neuron import ActivationFunctionTypes as aft
 
 neu = neuron.Neuron([1, 0.5], aft.LogisticFunction)
-# neu.updateWeights()
 
 for i in range(10):
     neu.train([1, -1], 0.25)
