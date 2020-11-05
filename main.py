2c82a84cba6e4bf0791e8fbba37c3fd8c58bbcc1
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-11-05 13:04:15 +0100

    [add] classifier

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import neuron
 from neuron import ActivationFunctionTypes as aft
+import classifier as c
 
 neu = neuron.Neuron([1, 0.5], aft.LogisticFunction)
 
@@ -7,3 +8,8 @@ for i in range(10):
     neu.train([1, -1], 0.25)
     neu.updateWeights()
     print()
+
+c1 = c.Classifier(2, 5)
+x, y = c1.getAllSamples()
+
+print(x, y)
